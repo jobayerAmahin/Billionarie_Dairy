ab0072a760296321ce638c482102d66ba45a2344
jobayerAmahin <ja.mahinmolla.2824@gmail.com>
2024-10-21 23:02:03 +0300

    Updating by adding a manual fake json data

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import GettingAPIData from './assets/API_Sources/bdAPI'
+import GettingAPIData from './assets/Components/API_Sources/bdAPI'
 
 function App() {
   const [count, setCount] = useState(0)
